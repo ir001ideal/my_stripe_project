40b1f11f76d7cdee856fb2c455f96a3592e6589c
debain <debain@vbox.myguest.virtualbox.org>
2025-11-07 10:31:33 -0600

    Final stable version, fixes all crashes

--- my_stripe_target.c
+++ my_stripe_target.c
@@ -1,7 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/bio.h> 
+#include <linux/bio.h>
 #include <linux/device-mapper.h>
 
 MODULE_LICENSE("GPL");
@@ -10,33 +10,42 @@ MODULE_DESCRIPTION("Odd/Even (Stripe) DM Target");
 
 struct my_stripe_context {
     struct dm_dev *even_dev;
-    struct dm_dev *odd_dev; 
+    struct dm_dev *odd_dev;
 };
 
 static int my_stripe_ctr(struct dm_target *ti, unsigned int argc, char **argv) {
     struct my_stripe_context *ctx = NULL;
+    fmode_t mode = FMODE_READ | FMODE_WRITE;
+
     if (argc != 2) {
         ti->error = "Invalid argument count";
         return -EINVAL;
     }
+
     ctx = kmalloc(sizeof(struct my_stripe_context), GFP_KERNEL);
     if (ctx == NULL) {
         ti->error = "Cannot allocate context";
         return -ENOMEM;
     }
-    if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &ctx->even_dev)) {
+
+    if (dm_get_device(ti, argv[0], mode, &ctx->even_dev)) {
         ti->error = "Cannot get EVEN device";
         kfree(ctx);
         return -EINVAL;
     }
-    if (dm_get_device(ti, argv[1], dm_table_get_mode(ti->table), &ctx->odd_dev)) {
+
+    if (dm_get_device(ti, argv[1], mode, &ctx->odd_dev)) {
         ti->error = "Cannot get ODD device";
         dm_put_device(ti, ctx->even_dev);
         kfree(ctx);
         return -EINVAL;
     }
+
     ti->private = ctx;
-    printk(KERN_INFO "my_stripe: constructor complete.\n");
+    ti->max_io_len = 1;
+    ti->num_flush_bios = 0;
+
+    printk(KERN_INFO "FINAL_FIX: constructor complete.\n");
     return 0;
 }
 
@@ -45,7 +54,7 @@ static void my_stripe_dtr(struct dm_target *ti) {
     dm_put_device(ti, ctx->even_dev);
     dm_put_device(ti, ctx->odd_dev);
     kfree(ctx);
-    printk(KERN_INFO "my_stripe: destructor complete.\n");
+    printk(KERN_INFO "FINAL_FIX: destructor complete.\n");
 }
 
 static int my_stripe_map(struct dm_target *ti, struct bio *bio) {
@@ -53,13 +62,15 @@ static int my_stripe_map(struct dm_target *ti, struct bio *bio) {
     sector_t logical_sector = bio->bi_iter.bi_sector;
     sector_t physical_sector;
 
-    if (logical_sector & 1) { 
+    if (logical_sector & 1) {
         bio->bi_bdev = ctx->odd_dev->bdev;
     } else {
         bio->bi_bdev = ctx->even_dev->bdev;
     }
-    physical_sector = logical_sector >> 1; 
+
+    physical_sector = logical_sector >> 1;
     bio->bi_iter.bi_sector = physical_sector;
+
     return DM_MAPIO_REMAPPED;
 }
 
@@ -77,14 +88,14 @@ static int __init my_stripe_init(void) {
     if (result < 0) {
         printk(KERN_ALERT "Failed to register my_stripe target\n");
     } else {
-        printk(KERN_INFO "my_stripe target registered.\n");
+        printk(KERN_INFO "FINAL_FIX: target registered.\n");
     }
     return result;
 }
 
 static void __exit my_stripe_exit(void) {
     dm_unregister_target(&my_stripe_target);
-    printk(KERN_INFO "my_stripe target unregistered.\n");
+    printk(KERN_INFO "FINAL_FIX: target unregistered.\n");
 }
 
 module_init(my_stripe_init);
